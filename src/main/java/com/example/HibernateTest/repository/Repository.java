cb91dc223fd692e1a6f1512866777c75b8310e7e
Maksim Muhomedyarov <madmaxim22@gmail.com>
2023-06-04 18:43:56 +0300

    feat: implements Repository

---
+++ src/main/java/com/example/HibernateTest/repository/Repository.java
@@ -0,0 +1,19 @@
+package com.example.HibernateTest.repository;
+
+import com.example.HibernateTest.entity.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
+@org.springframework.stereotype.Repository
+public class Repository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Person> getPersonsByCity(String city) {
+        String sql = "SELECT p FROM Person p WHERE p.cityOfLiving = :city";
+        return entityManager.createQuery(sql, Person.class).setParameter("city", city).getResultList();
+    }
+}
